ae05c8eee949ccdb6383404bd9e591278bd56931
Bethany Hull <bethanyhull@u.boisestate.edu>
2020-02-13 14:23:25 -0700

    code for testing getItems method

--- Project.java
+++ Project.java
@@ -34,10 +34,10 @@ public class Project {
 		 //TODO: Add querys here in switch statements
 			else if (args[0].equals( "test") ){
 				
-//			 	System.out.println("Running createItem");
-//			 	getItems(conn, args);
+			 	System.out.println("Running createItem");
+			 	getItems(conn, args);
 			 	
-				runQuery(conn);
+//				runQuery(conn);
 			}
 			else {
 				System.out.println("No process requested");
@@ -154,16 +154,53 @@ public class Project {
 			
 		}
 		
-		private static Statement getItems(Connection con, String[] data) throws SQLException {
+		private static void getItems(Connection conn, String[] data) throws SQLException {
 			String[] cleanData = removeFirstArg(data);
-			 String sql;
-			  java.sql.Statement stmt = null;
-			  stmt = con.createStatement();
+			String sql;
+			  sql = "CALL GetItems(\"" + cleanData[0] + "\");";
 			  
-			  sql = "CALL GetItems(" + cleanData[0] + ");";
-			  ResultSet res = stmt.executeQuery(sql);
-			  System.out.println(res);
-			  return stmt;
+			  Statement stmt = null;
+				ResultSet rs = null;
+			
+				try {
+					
+					
+					 
+					stmt = conn.createStatement();
+					rs = stmt.executeQuery(sql);  // no real code required... just a real db connection
+					// Now do something with the ResultSet ....
+					
+					rs.beforeFirst();
+					while (rs.next()) {
+						System.out.println(rs.getInt(1) 
+								+ ":" + rs.getString(2) 
+								+ ":" + rs.getString(3) 
+								+ ":" + rs.getString(4));
+					}
+
+				} catch (SQLException ex) {
+					// handle any errors
+					System.err.println("SQLException: " + ex.getMessage());
+					System.err.println("SQLState: " + ex.getSQLState());
+					System.err.println("VendorError: " + ex.getErrorCode());
+				} finally {
+					// it is a good idea to release resources in a finally{} block
+					// in reverse-order of their creation if they are no-longer needed
+					if (rs != null) {
+						try {
+							rs.close();
+						} catch (SQLException sqlEx) {
+						} // ignore
+						rs = null;
+					}
+					if (stmt != null) {
+						try {
+							stmt.close();
+						} catch (SQLException sqlEx) {
+						} // ignore
+						stmt = null;
+					}
+				}
 			
 		}
 		
