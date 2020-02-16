cdfce2af430153f2cc0ba2872e48756437a5dbb6
AshRivera <47310476+AshRivera@users.noreply.github.com>
2020-02-16 10:42:34 -0700

    Update Project.java
    edited getItems print statement

--- Project.java
+++ Project.java
@@ -255,16 +255,17 @@ public class Project {
 
 			rs.beforeFirst();
 
-			System.out.println(String.format("%1$-" + 5 + "s", "Item ID")
+			
+			System.out.println(String.format("%1$-" + 10 + "s", "Item ID")
 					+ String.format("%1$-" + 15 + "s", "Item Code") 
 					+ String.format("%1$-" + 20 + "s", "Item Description")
 					+ String.format("%1$-" + 5 + "s", "Item Price"));
 			while (rs.next()) {
-				System.out.println(String.format("%1$-" + 5 + "s", rs.getInt(1))
-						+ "+" + String.format("%1$-" + 15 + "s", rs.getString(2)) 
-						+ ":" + String.format("%1$-" + 20 + "s", rs.getString(3))
-						+ ":" + String.format("%1$-" + 5 + "s", rs.getString(4)));
-			}
+				System.out.println(String.format("  %1$-" + 10 + "s", rs.getInt(1))
+					+ String.format("%1$-" + 15 + "s", rs.getString(2)) 
+					+ String.format("%1$-" + 20 + "s", rs.getString(3))
+					+ String.format("%1$-" + 5 + "s", rs.getString(4)));
+			}	
 
 		} catch (SQLException ex) {
 			// handle any errors
